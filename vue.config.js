0acb6c40bf11bb37a4549712f179137a2bfdf1c9
samkokjam <89114987+samkokjam@users.noreply.github.com>
2022-06-21 14:48:58 +0700

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,32 @@
+// module.exports = {
+//   pwa: {
+//     workboxOptions: {
+//       skipWaiting: true
+//     }
+//   }
+// }
+
+const manifestJSON = require('./public/manifest.json')
+
+module.exports = {
+  pwa: {
+    icons: manifestJSON.icons,
+    iconPaths: manifestJSON.iconPaths,
+    themeColor: manifestJSON.theme_color,
+    workboxOptions: {
+      runtimeCaching: [{
+        urlPattern: new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'google-fonts',
+          expiration: {
+            maxEntries: 30
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }]
+    }
+  }
+}
